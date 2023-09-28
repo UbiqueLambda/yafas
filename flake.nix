48ea1074d37be248f3940f7991a766b6b5a6b760
PedroHLC ☭ <root@pedrohlc.com>
2023-09-28 13:41:54 -0300

    update

--- flake.nix
+++ flake.nix
@@ -1,35 +1,34 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    yafas.url = "github:UbiqueLambda/yafas";
+    yafas.url = "github:UbiqueLambda/yafas/next";
   };
 
   outputs = { nixpkgs, yafas, ... }@inputs:
     let
-      whatever = "AAAAaaaaaaaaa";
+      someOverlay = _prev: _final: { };
 
       step0 =
         yafas.allLinux nixpkgs
-          ({ pkgs, system }: { package.default = whatever; });
+          ({ pkgs, system }: { packages.default = pkgs.nixpkgs-fmt; formatter = pkgs.nixpkgs-fmt; });
 
       step1 = yafas.withAarch64Darwin nixpkgs
-        (_prev: { pkgs, ... }: { package.default = whatever; });
+        (_prev: { pkgs, ... }: { packages.default = pkgs.nixpkgs-fmt; formatter = pkgs.nixpkgs-fmt; });
 
       step2 = yafas.withOverlays
-        (_prev: { default = whatever; });
+        (_prev: { default = someOverlay; });
 
       step3 =
         yafas.withOverlay "cool"
-          (_prev: whatever);
+          (_prev: someOverlay);
 
       step4 =
         yafas.withUniversals
-          (_prev: { myLibs = whatever; });
+          (_prev: { lib.hash = "sha256-"; });
 
       step5 =
         yafas.map
-          (prev: prev // { myLibs = prev.myLibs // { }; });
-
+          (prev: prev // { lib.hash = prev.lib.hash + "YEAH"; });
 
       pipe = val: functions:
         let reverseApply = x: f: f x;
