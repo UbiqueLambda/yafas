22095c8b1d7d51bc9fcaab50876db24618e2f41a
Pedro Lara Campos <root@pedrohlc.com>
2023-09-28 13:41:44 -0300

    More functions; docs; and fixes (#1)

--- flake.nix
+++ flake.nix
@@ -3,8 +3,13 @@
   outputs = { self, systems }:
     let
       # lib
-      inherit (builtins) filter foldl' mapAttrs stringLength substring zipAttrsWith;
+      inherit (builtins) attrNames elemAt filter foldl' head isAttrs
+        length mapAttrs stringLength substring zipAttrsWith;
 
+      update =
+        a: b: if isAttrs b then recursiveUpdate a b else b;
+
+      # nixpkgs clones
       hasSuffix = suffix: content:
         let
           lenContent = stringLength content;
@@ -13,32 +18,66 @@
         lenContent >= lenSuffix
         && substring (lenContent - lenSuffix) lenContent content == suffix;
 
+      recursiveUpdateUntil = pred: lhs: rhs:
+        let
+          f = attrPath:
+            zipAttrsWith (n: values:
+              let here = attrPath ++ [ n ]; in
+              if length values == 1
+                || pred here (elemAt values 1) (head values) then
+                head values
+              else
+                f here values
+            );
+        in
+        f [ ] [ rhs lhs ];
+
+      recursiveUpdate = lhs: rhs:
+        recursiveUpdateUntil (path: lhs: rhs: !(isAttrs lhs && isAttrs rhs)) lhs rhs;
+
       # main functions
-      support = targets: accu: nixpkgs: applier:
+      support = targets: nixpkgs: applier:
         let
           eachSystem = system:
             let
               nestSystem = _: value: { ${system} = value; };
+
               systemOutputs = applier {
                 inherit system;
                 pkgs = nixpkgs.legacyPackages.${system};
               };
             in
             mapAttrs nestSystem systemOutputs;
-
         in
         zipAttrsWith
-          (_: values: foldl' (a: b: a // b) { } values)
+          (_: values: foldl' update { } values)
           (map eachSystem targets);
 
-      support' = target: nixpkgs: applier: accu:
-        support [ target ] accu nixpkgs (applier accu);
+      support' = targets: nixpkgs: applier: accu:
+        update accu (support targets nixpkgs (applier accu));
 
       withUniversal = output:
-        applier: accu: accu // ({ "${output}" = (accu.${output} or { }) // applier accu; });
+        applier: accu: accu // ({
+          "${output}" =
+            let
+              new = applier accu;
+            in
+            if isAttrs new then
+              update (accu.${output} or { }) new
+            else new;
+        });
 
       withNestedUniversal = output: name:
         applier: accu: accu // ({ "${output}" = (accu.${output} or { }) // { "${name}" = applier accu; }; });
+
+      findSystems = accu:
+        attrNames (accu.legacyPackages or accu.packages or accu.apps or accu.formatter or accu.devShells);
+
+      withNestedSystem = name: nixpkgs: applier: accu:
+        support (findSystems accu) accu nixpkgs
+          ({ pkgs, system }@combo: {
+            "${name}" = update accu.${name}.${system} (applier accu combo);
+          });
 
       # system list
       importedSystems = import systems;
@@ -46,35 +85,48 @@
       darwins = filter (hasSuffix "-darwin") importedSystems;
     in
     {
-      # constructors
-      allLinux = support linuxes { };
-      allDarwin = support darwins { };
-      allSystems = support (linuxes ++ darwins) { };
+      # Constructors
+      allLinux = support linuxes;
+      allDarwin = support darwins;
+      allSystems = support (linuxes ++ darwins);
 
-      # pkgs-dependent
-      withDarwin = support darwins;
-      withLinux = support linuxes;
+      # With per-system
+      withSystems = support';
+      withDarwin = support' darwins;
+      withLinux = support' linuxes;
 
-      withSystem = support';
-      withAMD64Linux = support' "x86_64-linux";
-      withAarch64Linux = support' "aarch64-linux";
-      withAMD64Darwin = support' "x86_64-darwin";
-      withAarch64Darwin = support' "aarch64-darwin";
+      withSystem = target: support' [ target ];
+      withAarch64Linux = support' [ "aarch64-linux" ];
+      withAMD64Linux = support' [ "x86_64-linux" ];
+      withAarch64Darwin = support' [ "aarch64-darwin" ];
+      withAMD64Darwin = support' [ "x86_64-darwin" ];
 
-      # universal attrset
+      # With per-system products
+      inherit withNestedSystem;
+      withApps = withNestedSystem "apps";
+      withDevShells = withNestedSystem "devShells";
+      withLegacyPackages = withNestedSystem "legacyPackages";
+      withPackages = withNestedSystem "packages";
+      withFormatter = nixpkgs: applier: accu:
+        support (findSystems accu) accu nixpkgs
+          (combo: { formatter = applier combo; });
+
+      # With universals
+      withUniversals = applier: accu: accu // (applier accu);
+      inherit withUniversal;
+      withHomeManagerModules = withUniversal "homeManagerModules";
+      withNixOSModules = withUniversal "nixosModules";
       withOverlays = withUniversal "overlays";
       withSchemas = withUniversal "schemas";
-      withNixOSModules = withUniversal "nixosModules";
-      withHomeManagerModules = withUniversal "homeManagerModules";
-      withUniversals = applier: accu: accu // (applier accu);
 
-      # universal products
+      # With universals products
+      inherit withNestedUniversal;
+      withHomeManagerModule = withNestedUniversal "homeManagerModules";
+      withNixOSModule = withNestedUniversal "nixosModules";
       withOverlay = withNestedUniversal "overlays";
       withSchema = withNestedUniversal "schemas";
-      withNixOSModule = withNestedUniversal "nixosModules";
-      withHomeManager = withNestedUniversal "homeManagerModules";
 
-      # helper
+      # Helpers
       map = applier: accu: applier accu;
     };
 }
